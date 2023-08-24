7f18c52e3b6b8275f8676ed38ccbc355729ac37e
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-24 19:36:43 +0200

    complete ex 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,60 +13,7 @@ La risposta finale (o output) sarà anch’essa da scrivere in solo console.
 
 */
 
-// chiedi il nome
-
-//const userName = prompt('Come ti chiami?', 'nome cognome');
-
-// chiedi i km da percorrere
-
-//const kmToDo = Number(prompt('Quanti km vuoi percorrere?'));
-
-// chiedi età utente
-
-//const userAge = Number(prompt('Quanti anni hai?'));
-
-//console.log(kmToDo, userAge);
-
-//calcolare prezzo ( a km = 0.21 €)
-
-
-
-//let travelPrice = Number(kmToDo * pricePerKm );
-
-//console.log(travelPrice);
-
-//let message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' €'
-
-// verifica se minore di 18 👉 sconto 20%
-// verifica se maggiore di 65 👉 sconto 40%
-
-
-
-
-/* if (userAge < 18) {
-    travelPrice = (travelPrice - (travelPrice * 0.2))
-    //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 20% ( under 18 )'
-
-} else if (userAge > 65) {
-    travelPrice = (travelPrice - (travelPrice * 0.4))
-    //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 40% ( over 65 )'
-}
-
-console.log(message); */
-
-//document.getElementById('nome').value = userName
-
-//document.getElementById('km').value = kmToDo
-
-/* 
-document.querySelector('button').innerHTML = 'Maggiorenne'
-document.querySelector('button').innerHTML = 'Minorenne'
-document.querySelector('button').innerHTML = 'Over 65'
-*/
-
-/* 
-
-MILESTONE 2:
+/* MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.
@@ -78,24 +25,9 @@ per semplificarvi un pó la vita almeno per ora non usate il tag form per racchi
 Bonus (opzionale):
 Prova a racchiudere gli input ed il bottone in un tag form e prova a far funzionare il form evitando che la pagina si refreshi quando il form viene inviato cliccando su genera.
 Questo richiederá un minimo di ricerca per capire come usare il parametro e dentro la funzione anonima dell'event listener. function(e){ console.log(e) }
-Ricordate, molte cose nella vita se fatte per la prima volta risultano difficili se non quasi impossibili, ma é perseverando e soprattutto sbagliando che impariamo. Che la forza sia con voi!*/
-
-/* const generate = document.getElementById('generate').addEventListener('click', function() {
-    console.log('demo');
-    const inputElement = document.getElementById('')
-    console.log();
-}) */
+Ricordate, molte cose nella vita se fatte per la prima volta risultano difficili se non quasi impossibili, ma é perseverando e soprattutto sbagliando che impariamo. Che la forza sia con voi! */
 
 
-/* const kmToDo = document.getElementById('km').value;
-console.log(kmToDo);
-
-const userAge = document.getElementById('age').selectedIndex;
-const selectAge = document.getElementById("age").options;
-console.log( selectAge[userAge].text);
-
-const userName = document.getElementById('nome').value;
-console.log(userName); */
 
 
 const vai = document.getElementById('vai').addEventListener('click', function () {
@@ -114,34 +46,22 @@ const vai = document.getElementById('vai').addEventListener('click', function ()
     
     let travelPrice = pricePerKm * kmToDo;
 
-    if (userAge.selectedIndex == 'Minorenne') {
+    if (userAge == '1') {
         travelPrice = (travelPrice - (travelPrice * 0.2));
-        //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 20% ( under 18 )'
+        
     
-    } else if (selectAge.options == 'Over 65') {
+    } else if (userAge == '2') {
         travelPrice = (travelPrice - (travelPrice * 0.4));
-        //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 40% ( over 65 )'
-    } else {
-
+        
     }
 
-    console.log(travelPrice + ' €');
+    console.log(travelPrice.toFixed(2) + ' €');
 })
 
-/* const userAge = document.getElementById('age').selectedIndex;
-const selectAge = document.getElementById("age").options;
+const reset = document.getElementById('reset').addEventListener('click', function () {
 
-const kmToDo = Number(document.getElementById('km').value); */
-
+    location.reload();
 
 
-
-/* if (userAge == 'minorenne') {
-    travelPrice = (travelPrice - (travelPrice * 0.2));
-    //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 20% ( under 18 )'
-
-} else if (userAge == 'Over 65') {
-    travelPrice = (travelPrice - (travelPrice * 0.4));
-    //message = 'Il costo del biglietto è ' + travelPrice.toFixed(2) + ' € ' + 'applicando lo sconto del 40% ( over 65 )'
-} */
+})
 
